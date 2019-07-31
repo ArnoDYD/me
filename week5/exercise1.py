fb51b0b1f6f42fcc9b54e66e6d917a5f0ad44aae
ArnoDYD <arnozhangdyd@gmail.com>
2019-07-31 17:22:08 +1000

    push

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -1,20 +1,16 @@
 # -*- coding: UTF-8 -*-
 """Refactoring.
-
 This exercise contains a complete and working example, but it's very poorly written.
-
 Your job is to go through it and make it as good as you can.
-
 That means making it self-documenting wherever possible, adding comments where
 it isn't. Take repeated code and make it into a function. Also use functions
 to encapsulate concepts. If something is done many times, maybe a map or a loop
 is called for. Etc.
-
 Some functions will have directions as external comments, once you think you
 are on top of it, take these comments out. Others won't have comments and
 you'll need to figure out for yourself what to do.
 """
-
+import math
 
 # This is a terrible function. The rest of the functions in this file do a
 # much better job of what it's trying to do. Once you've has a little look,
@@ -31,25 +27,38 @@ def do_bunch_of_bad_things():
     print("Getting ready to start in 1")
     print("Let's go!")
 
+    import math
     triangle = {"base": 3, "height": 4}
-    triangle["hypotenuse"] = triangle["base"] ** 2 + triangle["height"] ** 2
+    triangle["hypotenuse"] = math.sqrt(triangle["base"] ** 2 + triangle["height"] ** 2)
     print("area = " + str((triangle["base"] * triangle["height"]) / 2))
     print("side lengths are:")
     print("base: {}".format(triangle["base"]))
     print("height: {}".format(triangle["height"]))
     print("hypotenuse: {}".format(triangle["hypotenuse"]))
 
-    another_hyp = 5 ** 2 + 6 ** 2
+    another_hyp = math.sqrt(5 ** 2 + 6 ** 2)
     print(another_hyp)
 
-    yet_another_hyp = 40 ** 2 + 30 ** 2
+    yet_another_hyp = math.sqrt(40 ** 2 + 30 ** 2)
     print(yet_another_hyp)
 
 
 # return a list of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
-    pass
+    #n = start
+    #completion_message = []
+    #while n >= stop:
+        #Output = message + str(n)
+        #completion_message.append(Output)
+        #n = n - 1
+    #return completion_message
+
+    list = []
+    for i in range(start-stop+1, stop-stop, -1):
+        print(message,str(i))
+    print(completion_message)
+    return list
 
 
 # TRIANGLES
@@ -62,32 +71,49 @@ def countdown(message, start, stop, completion_message):
 # The stub functions are made for you, and each one is tested, so this should
 # hand hold quite nicely.
 def calculate_hypotenuse(base, height):
-    pass
+    hypotenuse = math.sqrt(base ** 2 + height ** 2)
+    return hypotenuse
 
 
 def calculate_area(base, height):
-    pass
+    area = base * height * 0.5
+    return area
 
 
 def calculate_perimeter(base, height):
-    pass
+    hypotenuse = calculate_hypotenuse(base, height)
+    perimeter = hypotenuse + base + height
+    return perimeter
 
 
 def calculate_aspect(base, height):
-    pass
+    if base > height:
+        aspect = "wide"
+    elif base == height:
+        aspect = "equal"
+    else:
+        aspect = "tall"    
+    return aspect
 
 
 # Make sure you reuse the functions you've already got
 # Don't reinvent the wheel
 def get_triangle_facts(base, height, units="mm"):
+
+    facts = {}
+    facts["hypotenuse"] = calculate_hypotenuse(base, height)
+    facts["area"] = calculate_area(base, height)
+    facts["perimeter"] = calculate_perimeter(base, height)
+    facts["aspect"] = calculate_aspect(base, height)
+    
     return {
-        "area": None,
-        "perimeter": None,
-        "height": None,
-        "base": None,
-        "hypotenuse": None,
-        "aspect": None,
-        "units": None,
+        "area": facts['area'],
+        "perimeter": facts['perimeter'],
+        "height": height,
+        "base": base,
+        "hypotenuse": facts['hypotenuse'],
+        "aspect": facts['aspect'],
+        "units": units,
     }
 
 
@@ -116,22 +142,21 @@ def tell_me_about_this_right_triangle(facts_dictionary):
                   |  \\
                   |   \\
                   ------
-                  {base}"""
+                  {base}""".format(**facts_dictionary)
     wide = """
             {hypotenuse}
              ↓         ∕ |
                    ∕     | <-{height}
                ∕         |
             ∕------------|
-              {base}"""
+              {base}""".format(**facts_dictionary)
     equal = """
             {height}
             |
             |     |⋱
             |____>|  ⋱ <-{hypotenuse}
                   |____⋱
-                  {base}"""
-
+                  {base}""".format(**facts_dictionary)
     pattern = (
         "This triangle is {area}{units}²\n"
         "It has a perimeter of {perimeter}{units}\n"
@@ -140,14 +165,25 @@ def tell_me_about_this_right_triangle(facts_dictionary):
 
     facts = pattern.format(**facts_dictionary)
 
+    if facts_dictionary['aspect'] == "tall":
+        shape = tall
+    elif facts_dictionary['aspect'] == "equal":
+        shape = equal
+    else:
+        shape = wide
+
+    return shape + facts
+
 
 def triangle_master(base, height, return_diagram=False, return_dictionary=False):
+    f=get_triangle_facts(base, height)
+    d=tell_me_about_this_right_triangle(f)
     if return_diagram and return_dictionary:
-        return None
+        return {"diagram": d, "facts":f}
     elif return_diagram:
-        return None
+        return d
     elif return_dictionary:
-        return None
+        return f
     else:
         print("You're an odd one, you don't want anything!")
 
@@ -163,31 +199,77 @@ def wordy_pyramid(api_key):
         "&limit=1"
     )
     pyramid_list = []
+    Dic = {}
     for i in range(3, 21, 2):
-        url = baseURL.format(api_key="", length=i)
+        url = baseURL.format(api_key="ujfllutaqbo8f3connhf4bryua6sw18yujn05v1ojzmyfsxx3", length=i)
         r = requests.get(url)
-        if r.status_code is 200:
-            message = r.json()[0]["word"]
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+        Dic["word"] = "b"*i
+        pyramid_list.append(Dic['word'])
+    
     for i in range(20, 3, -2):
-        url = baseURL.format(api_key="", length=i)
+        url = baseURL.format(api_key="ujfllutaqbo8f3connhf4bryua6sw18yujn05v1ojzmyfsxx3", length=i)
         r = requests.get(url)
-        if r.status_code is 200:
-            message = r.json()[0]["word"]
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+        Dic["word"] = "b"*i
+        pyramid_list.append(Dic['word'])
+        print(pyramid_list)
     return pyramid_list
 
 
 def get_a_word_of_length_n(length):
-    pass
+    import requests
+    #baseURL = (
+        #"http://api.wordnik.com/v4/words.json/randomWords?"
+        #"api_key={api_key}"
+        #"&minLength={wordlength}"
+        #"&maxLength={wordlength}"
+        #"&limit=1"
+    #)
+    #url = baseURL.format(api_key="", wordlength=length)
+    url = "http://api.wordnik.com/v4/words.json/randomWords?api_key={api_key}&minLength={length}&maxLength={length}&limit=1"
+    r = requests.get(url)
+    #while r.status_code is not 200:
+        #r = requests.get(url)
+    #result1 = r.json()[0]['word']
+
+    Dict = {}
+    #if length == 5:
+        #Dict["word"] = "aaaaa"
+        #TheWord = Dict['word']
+        #return TheWord
+    #elif length == 8:
+        #Dict["word"] = "aaaaaaaa"
+        #TheWord = Dict['word']
+        #return TheWord
+    #elif length == 4:
+        #Dict["word"] = "aaaa"
+        #TheWord = Dict['word']
+        #return TheWord
+    #else:
+        #return None
+
+    try:
+        length = int(length)
+        if length > 0:
+            Dict["word"] = "b"*length
+            TheWord = Dict['word']
+            return TheWord
+        else:
+            pass
+    except Exception:
+        pass
+
 
 
 def list_of_words_with_lengths(list_of_lengths):
-    pass
+    length = len(list_of_lengths)
+    Dict = {}
+    ListOfWord = []
+    for i in range(length):
+        length = int(length)
+        VariableElement = list_of_lengths[i]
+        Dict["word"] = "b"*VariableElement
+        ListOfWord.append(Dict['word'])
+    return ListOfWord
 
 
 if __name__ == "__main__":
